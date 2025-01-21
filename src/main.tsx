3943cb4f9274d8f6f383f4fee5ed5a251798f688
CinnamonfirJ <cinnamonfir@gmail.com>
2025-01-21 01:16:02 +0100

    edited env

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import process from "process";
+// import process from "process";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { LoginProvider } from "./context/LoginContext.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
-// const oAuthClientID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string;
-console.log("Environment Variable:", process.env.REACT_APP_GOOGLE_CLIENT_ID);
-const OAuthClientID =
-  "990820965273-2k75plcnsthraoj5b91adubs09l806sv.apps.googleusercontent.com";
+const OAuthClientID = import.meta.env.VITE_OAUTH_CLIENT_ID;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
